980baa6c1dfb7b50859cfb622af18833d1dee216
jzfrank <frankjinzhang@gmail.com>
2022-11-09 08:07:48 +0100

    run Q4 with rep 100, but way too slow

--- Q4_nct.m
+++ Q4_nct.m
@@ -3,14 +3,14 @@ warning("off", "all");
 randSeed = "default";
 rng(randSeed);
 % loc = 1; scale = 2; 
-mu = -2; df = 4; alpha = 0.01; % possibly new paramters 
+mu = -3; df = 3; alpha = 0.01; % possibly new paramters 
 trueES_nct1 = theoretical_ES_NCT_direct_integration(df, mu, alpha);
 trueES_nct2 = theoretical_ES_NCT_from_simulation(df, mu, alpha, 1e7);
 trueES_t = theoretical_ES_t(0, 1, df, alpha);
 % when mu = 0, nct should be same as t 
 disp([trueES_nct1, trueES_nct2, trueES_t]);
 % 
-rep = 1; % parametric bootstrap mle is too slow
+rep = 100; % parametric bootstrap mle is too slow
 
 file_name = strcat('Q4_diary', '_rep', num2str(rep), '_df', num2str(df), ...
     '_mu', num2str(mu), '_alpha', num2str(alpha), ...
@@ -18,7 +18,7 @@ file_name = strcat('Q4_diary', '_rep', num2str(rep), '_df', num2str(df), ...
 diary(file_name);
 
 disp(["rep", rep, "df", df, "mu", mu, "alpha", alpha, "randSeed", randSeed]);
-for T=[250, 500, 2000]
+for T=[100, 500, 2000]
     tic;
     [coverage_accuracy, avg_interval_length] = ...
         nonparametric_bootstrap_ES_nct(T, rep, df, mu, alpha, trueES_nct1);
